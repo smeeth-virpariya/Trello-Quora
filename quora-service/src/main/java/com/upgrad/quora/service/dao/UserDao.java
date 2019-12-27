590a09915efefd3bbae79fa6facefb176d316a8c
ShwetaYadav19 <shwetuyadav19@gmail.com>
2019-12-27 17:34:53 +0530

    Adding delete user feature for admin users

--- quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -26,4 +26,24 @@ public class UserDao {
             return null;
         }
     }
+
+    /**
+     * Delete a user by given id from the DB.
+     *
+     * @param userId Id of the user whose information is to be fetched.
+     * @return User details which is to be deleted if exist in the DB else null.
+     */
+
+    public UserEntity deleteUser(final String userId){
+        try {
+            UserEntity deleteUser = this.entityManager.createNamedQuery( "userByUserId", UserEntity.class ).
+                    setParameter( "userId", userId )
+                    .getSingleResult();
+            this.entityManager.remove( deleteUser );
+            return deleteUser;
+        } catch (NoResultException nre){
+            return null;
+        }
+
+    }
 }
