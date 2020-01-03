73c11cecbd9e7a4e4653ce4655b01f9a8b0de7bb
ShwetaYadav19 <shwetuyadav19@gmail.com>
2020-01-03 20:33:35 +0530

    Adding endpoint to get all questions posted by a specific user

--- quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -2,7 +2,6 @@ package com.upgrad.quora.api.controller;
 
 import com.upgrad.quora.api.model.*;
 import com.upgrad.quora.service.business.QuestionService;
-import com.upgrad.quora.service.dao.QuestionDao;
 import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
@@ -32,12 +31,12 @@ public class QuestionController {
     @RequestMapping(method = RequestMethod.POST, path = "/question/create", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<QuestionResponse> createQuestion(@RequestHeader("authorization") final String accessToken, QuestionRequest questionRequest) throws AuthorizationFailedException {
         QuestionEntity questionEntity = new QuestionEntity();
-        questionEntity.setContent(questionRequest.getContent());
-        questionEntity = questionService.createQuestion(questionEntity, accessToken);
+        questionEntity.setContent( questionRequest.getContent() );
+        questionEntity = questionService.createQuestion( questionEntity, accessToken );
         QuestionResponse questionResponse = new QuestionResponse();
-        questionResponse.setId(questionEntity.getUuid());
-        questionResponse.setStatus("QUESTION CREATED");
-        return new ResponseEntity<QuestionResponse>(questionResponse, HttpStatus.CREATED);
+        questionResponse.setId( questionEntity.getUuid() );
+        questionResponse.setStatus( "QUESTION CREATED" );
+        return new ResponseEntity<QuestionResponse>( questionResponse, HttpStatus.CREATED );
     }
 
     /**
@@ -49,15 +48,40 @@ public class QuestionController {
      */
     @RequestMapping(method = RequestMethod.GET, path = "/question/all", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<List<QuestionDetailsResponse>> getAllQuestions(@RequestHeader("authorization") final String accessToken) throws AuthorizationFailedException {
-        List<QuestionEntity> questions = questionService.getAllQuestions(accessToken);
+        List<QuestionEntity> questions = questionService.getAllQuestions( accessToken );
         List<QuestionDetailsResponse> questionDetailResponses = new ArrayList<>();
         for (QuestionEntity questionEntity : questions) {
             QuestionDetailsResponse questionDetailResponse = new QuestionDetailsResponse();
-            questionDetailResponse.setId(questionEntity.getUuid());
-            questionDetailResponse.setContent(questionEntity.getContent());
-            questionDetailResponses.add(questionDetailResponse);
+            questionDetailResponse.setId( questionEntity.getUuid() );
+            questionDetailResponse.setContent( questionEntity.getContent() );
+            questionDetailResponses.add( questionDetailResponse );
         }
-        return new ResponseEntity<List<QuestionDetailsResponse>>(questionDetailResponses, HttpStatus.OK);
+        return new ResponseEntity<List<QuestionDetailsResponse>>( questionDetailResponses, HttpStatus.OK );
+    }
+
+
+    /**
+     * Get all questions posted by a user with given userId.
+     *
+     * @param userId      of the user for whom we want to see the questions asked by him
+     * @param accessToken access token to authenticate user.
+     * @return List of QuestionDetailsResponse
+     * @throws AuthorizationFailedException In case the access token is invalid.
+     */
+
+    @RequestMapping(method = RequestMethod.GET, path = "question/all/{userId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<QuestionDetailsResponse>> getQuestionByUserId(@PathVariable("userId") String userId,
+                                                                             @RequestHeader("authorization") final String accessToken) throws AuthorizationFailedException {
+
+        List<QuestionEntity> questions = questionService.getAllQuestionsByUser( userId, accessToken );
+        List<QuestionDetailsResponse> questionDetailResponses = new ArrayList<>();
+        for (QuestionEntity questionEntity : questions) {
+            QuestionDetailsResponse questionDetailResponse = new QuestionDetailsResponse();
+            questionDetailResponse.setId( questionEntity.getUuid() );
+            questionDetailResponse.setContent( questionEntity.getContent() );
+            questionDetailResponses.add( questionDetailResponse );
+        }
+        return new ResponseEntity<List<QuestionDetailsResponse>>( questionDetailResponses, HttpStatus.OK );
     }
 
     /**
@@ -72,11 +96,12 @@ public class QuestionController {
      */
     @RequestMapping(method = RequestMethod.PUT, path = "/question/edit/{questionId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<QuestionEditResponse> editQuestion(@RequestHeader("authorization") final String accessToken, @PathVariable("questionId") final String questionId, QuestionEditRequest questionEditRequest) throws AuthorizationFailedException, InvalidQuestionException {
-        QuestionEntity questionEntity = questionService.editQuestion(accessToken, questionId, questionEditRequest.getContent());
+        QuestionEntity questionEntity = questionService.editQuestion( accessToken, questionId, questionEditRequest.getContent() );
         QuestionEditResponse questionEditResponse = new QuestionEditResponse();
-        questionEditResponse.setId(questionEntity.getUuid());
-        questionEditResponse.setStatus("QUESTION EDITED");
-        return new ResponseEntity<QuestionEditResponse>(questionEditResponse, HttpStatus.OK);
+        questionEditResponse.setId( questionEntity.getUuid() );
+        questionEditResponse.setStatus( "QUESTION EDITED" );
+        return new ResponseEntity<QuestionEditResponse>( questionEditResponse, HttpStatus.OK );
     }
+
 
 }
--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
@@ -2,8 +2,10 @@ package com.upgrad.quora.service.business;
 
 import com.upgrad.quora.service.dao.QuestionDao;
 import com.upgrad.quora.service.dao.UserAuthDao;
+import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,9 @@ public class QuestionService {
 
     @Autowired
     private UserAuthDao userAuthDao;
+
+    @Autowired
+    private UserDao userDao;
 
     @Autowired
     private QuestionDao questionDao;
@@ -88,5 +93,25 @@ public class QuestionService {
         questionEntity.setContent(content);
         questionDao.updateQuestion(questionEntity);
         return questionEntity;
+    }
+
+    /**
+     * Gets all the questions posted by a specific user.
+     *
+     * @param  userId userId of the user whose posted questions have to be retrieved
+     * @param accessToken accessToken of the user for valid authentication.
+     * @return List of QuestionEntity
+     * @throws AuthorizationFailedException ATHR-001 - if user token is not present in DB. ATHR-002 if the user has already signed out.
+     */
+    public List<QuestionEntity> getAllQuestionsByUser(final String userId, final String accessToken) throws AuthorizationFailedException {
+        UserAuthEntity userAuthEntity = userAuthDao.getUserAuthByToken(accessToken);
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to get all questions posted by a specific user");
+        }
+        UserEntity user = userDao.getUserById( userId );
+
+        return questionDao.getAllQuestionsByUser(user);
     }
 }
